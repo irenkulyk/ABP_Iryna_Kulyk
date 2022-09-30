7b0a35a82764a36dbf2ad4d1da69dc42de8b411f
Iryna Kulyk <irynakulyk1532@gmail.com>
2022-09-30 18:14:01 +0300

    Iryna

--- Lab_1/main.cpp
+++ Lab_1/main.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 
 using namespace std;
+int main() {
+    float x, y;
+    int choice;
+    //cin >> choice;
+    switch (choice)
+    {
+     case 1:
+            cout << "x =" << endl;
+            cin >>x;
+            cout << "y=" << endl ;
+            cin >>y;
+            break;
+        case 2:
+            if (x >= -1 && x <= 1 && y >= -1 && y <= 1 && ! (x > 0 && y < 0))
+                cout << "This point falls into the area";
+
+        case 3: exit(0);
+    }
+    cout << "Enter x:  \n";
+    cin >> x;
+
+    cout << "Enter y:  \n";
+    cin >> y;
+    if (x >= -1 && x <= 1 && y >= -1 && y <= 1)
+        cout << "This point falls into the area";
+    else
+        cout << "The point doesn't fall into the area";
+    return 0;
+}
 
 
