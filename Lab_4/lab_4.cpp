e5951c94fca7b8c0aa3bf1cdbefeecfbd5dfec26
Iryna Kulyk <irynakulyk1532@gmail.com>
2022-11-09 18:14:30 +0200

    Iryna

--- Lab_4/lab_4.cpp
+++ Lab_4/lab_4.cpp
@@ -2,7 +2,6 @@
 #include <iomanip>
 using namespace std;
 
-
 int main() {
     int a[9][9], b[9][9], i, j, n = 9, m = 9;
     cout << "\n";
@@ -18,11 +17,28 @@ int main() {
         for(j =0; j< m; j++)
             b[i][j] = a[i][j];
     cout << "\n";
-    for (i = 0; i < n; i++)
+
+    for (i = 0; i < n; i++){
         for(j = 0; j < m; j++)
             cout <<b[i][j] << " ";
         cout << "\n";
+    }
 
-
+    void masSum(int array[9][9], int mas[9]) {
+    int Sum;
+        for (int i = 0; i < n; i++) {		// зовнішній цикл по рядкам
+            Sum = 0;
+            for (int j = 0; j < m; j++) {	// внутрішній цикл по стовпцям
+                Sum += a[i][j];			// шукаємо суму елементів рядка
+            }
+            mas [i] = Sum;					// додаємо в одновимірний масив суму відповідного рядка
+            cout << mas[i] << " ";
+        }
+    }
     return 0;
 }
+
+
+
+
+
--- Lab_6/lab_6.cpp
+++ Lab_6/lab_6.cpp
@@ -4,34 +4,27 @@ using namespace std;
 
 
 int main() {
-
-    int arraySize;
-
-    cout << "Enter array size :" << endl;
-    cin >> arraySize;
-    int *arrayOne = new int(arraySize);
-    int s[9][9];
-    int size[81];
-    srand(time(0));
-    int  i, j, n = 9, m = 9;
+    int x[9][9], i, j, n = 9, m = 9;
     cout << "\n";
     for (i = 0; i < n; i++)
         for(j = 0; j < m; j++)
-            s[i][j] = rand() % 50;
+            x[i][j] = rand() % 50;
     for (i = 0; i < n; i++){
         for(j = 0; j< n; j++)
-            cout << s[i][j] << " ";
+            cout << x[i][j] << " ";
         cout << "\n";
     }
     for (i = 0; i < n; i++) {
         for(j = 0; j < n; j++) {
             if(i != j){
-                s[i][j] = s[j][i];
+                x[i][j] = x[j][i];
             }
-            cout << setw(5) << s[i][j];
+            cout << setw(5) << x[i][j];
         }
         cout << endl;
     }
     return 0;
 }
+
+
 
