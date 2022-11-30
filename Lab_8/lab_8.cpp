11339a9193a510e2e7471f27cc94fdbe56b07745
Iryna Kulyk <irynakulyk1532@gmail.com>
2022-11-30 19:55:20 +0200

    Iryna

--- Lab_8/lab_8.cpp
+++ Lab_8/lab_8.cpp
@@ -1,84 +1,64 @@
 #include <iostream>
 #include <iomanip>
+#include <string.h>
 #include <algorithm>
 
 
 using namespace std;
- struck Srat1 {
-     long int Number;
-     double Weight;
-     double Part;
-     char Class;
-     bool operator <(const Star1 & other) const {
-         return Class < other.Class;
-     }
-
- };
- struct Stack {
-     Star1 star;
-     Stack *next;
- };
- Stack *elem ,*stack;
- void cinStack(Stack *stack);
- void coutArray(Star1* array, int *size);
- void StackToArray(Stack *stack, Star1* array, int* size);
-
- int main(){
-     SetConcoleOutCP(CP_UTF8);//
-     stack = NULL;
-     Star1 starArray [10];
-     int sizeValue = 0;
-     int *size = &sizeValue;
-
-     cinStack(stack);
-     StackToArray(stack, starArray, size);
-     coutArray(starArray, size);
-     return 0;
- }
-void cinStack(Stack *stack) {
-    string answer = "";
-    do {
-        char Class;
-        double Weight, Part;
-        long long int Number;
-
-        elem = new(Stack);
-        elem->next = stack;
-        stack = elem;
-
-        cout << "Enter: Class, Weight, Part, Number " << endl;
-        cin >> Class >> Weight >> Part >> Number;
-        cout << "One more?(+/-)" << endl;
-
-        Star1 currentStar;
-        currentStar.Class = Class;
-        currentStar.Weight = Weight;
-        currentStar.Part = Part;
-        currentStar.Number = Number;
-
-        elem->star = currentStar;
-
-        cin >> answer;
-    } while (answer == "+");
-
-
-    Star1 currentStar = array[i];
-
-    cout << "|" << setw(5) << left << currentStar.Class << "|" << setw(14) << left << currentStar.Weight << "   |"
-         << setw(7) << left << currentStar.Part << "   |"
-    setw(9) << left << currentStar.Number << "|"
-    setw(6) << endl;
+typedef struct{
+   char Part[5];
+   char Weight[4];
+   int Number;
+   float Spectralclass;
+}DATA;
+typedef struct tag_obj{
+    DATA d;
+    struct tag_obj* next;
+}OBJ;
+tag_obj* top = NULL;
+tag_obj* push(DATA d){
+    OBJ* ptr = new OBJ;
+    ptr->d = d;
+    ptr->next = top;
+    top = ptr;
+    return ptr;
 }
-cout << "-----------------------------------------------------------------------------\n ";
+void pop() {
+    if (top != NULL) {
+        OBJ *ptr = top->next;
+        cout << "Delete is " << top->d.Part<< endl;
+        delete top;
+        top = ptr;
+    }
 }
-void StrackToArray(Strack* stack, Star1* array, int* size) {
-    int i = 0;
-    do{
-        Star1 currentStar = elem->star;
-        array[i] = currentStar;
-        i++;
-        stack = elem->next;
-        elem = stack;
-        *size = i;
-    }while (stack != NUll);
+void show(){
+    OBJ* a = top;
+    cout << " -----------------------------------------------------------------------------\n ";
+    cout << "|  Approximate number of stars of different spectral classes in Galaxy      |\n ";
+    cout << "-----------------------------------------------------------------------------\n ";
+    cout << "|      Spectral class      |    Weight        |     Part    |     Number    |\n ";
+    cout << "-----------------------------------------------------------------------------\n ";
+    while (a!= NULL){
+        cout << "|"<<setw(25)<<left<<a->d.Spectralclass<<"|"<<setw(16)<<left<<a->d.Weight<<setw(11)<<left<<a->d.Part<<setw(13)<<left<<a->d.Number<<"|"<< endl;
+        cout << endl;
+        a = a->next;
+    }
+    cout << " ----------------------------------------------------------------------------\n ";
+    cout << "|               Note: data not shown for classes: B, A, G, K                |\n ";
+    cout << "-----------------------------------------------------------------------------\n ";
+
 }
+int main() {
+    DATA a;
+    for (int i = 0; i < 2; i++) {
+    cout << i + 1 << ".Enter: Spectral class, Weight, Part, Number \n";
+    cin >> a.Spectralclass >> a.Weight >> a.Number >> a.Part;
+    push(a);
+    }
+show ();
+}
+/*
+  R 32 0.1 5500
+  F 15 2.9 1200
+  M 50 3.2 2930
+ */
